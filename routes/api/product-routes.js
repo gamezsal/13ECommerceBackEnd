a08670692a5cf503f41a6bad2099afb96e40cb1d
Sal Gamez <gamezsalvador@gmail.com>
2020-12-28 21:48:40 -0700

    started to add delete and get routes to product-routes.js files

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,28 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const allData = await Product.findAll({
+      include: [{ model: Category }, { model: Tag}],
+    });
+    res.status(200).json(allData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const data = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag}],
+    });
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
@@ -91,6 +107,20 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  try {
+    const data = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if(!data) {
+      res.status(400).json({message: "Product id was not found, nothing has been deleted."})
+      return;
+    }
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 module.exports = router;
