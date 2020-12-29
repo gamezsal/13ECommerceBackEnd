58d469bcf3479cecf5e9ce9cef90edc267df3b18
Sal Gamez <gamezsalvador@gmail.com>
2020-12-28 23:24:35 -0700

    ran npm run seed, made sure it ran, and populated data in db

--- models/Product.js
+++ models/Product.js
@@ -11,10 +11,10 @@ Product.init(
   {
     // define columns
     id: {
-      autoIncrement: true,
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
+      autoIncrement: true,
     },
     product_name: {
       type: DataTypes.STRING,
