cc0c8d076f7a799484ecc19e1a22b28a6aaea4a6
Sal Gamez <gamezsalvador@gmail.com>
2020-12-28 22:32:58 -0700

    added more async routes ot tag and product, set sequlized async in server.js file force: false, as it was causing an error everytimne the app would restart

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,21 +4,21 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
-    const allData = await Product.findAll({
+    const data = await Product.findAll({
       include: [{ model: Category }, { model: Tag}],
     });
-    res.status(200).json(allData);
+    res.status(200).json(data);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
@@ -105,7 +105,7 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
   try {
     const data = await Product.destroy({
